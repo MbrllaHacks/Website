abca6fbee420da77ccb4040095dc7fd38f4907e5
MohammedAl-Rasheed <mohammed.satar.alrasheed@gmail.com>
2022-07-01 20:52:12 +0400

    Set up footer components

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import logo from './../../Assets/logo.svg'
+import a from './../../Assets/a.svg'
+import disc from './../../Assets/disc.svg'
+import twitter from './../../Assets/twitter.svg'
+import insta from './../../Assets/instagram.svg'
+import "./favbar.css"
+
+
+function FooteerComp(){
+  return ( 
+    <div>
+
+    </div>
+  )
+}
+
+export default FooteerComp
