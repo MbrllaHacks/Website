139ebcc0bf6014df8322842f7542545536b0c192
Amisha Sao <mintysao@gmail.com>
2022-07-09 22:59:03 -0500

    git commit src/components/Faq/faq.css git commit src/components/Faq/faq.js git commit src/components/Faq/questions.js

--- src/components/Faq/questions.js
+++ src/components/Faq/questions.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col} from "react-bootstrap";
-import './faq.css';
-import { useRef } from "react";
+import './questions.css';
 
 class ToggleableAnswer extends React.Component{
     constructor(props){
@@ -11,22 +10,36 @@ class ToggleableAnswer extends React.Component{
         this.isClicked = this.isClicked.bind(this);
     }
 
+    componentDidMount(){
+        this.expandText()
+    }
+
     isClicked(){
         this.setState(previousState => ({
             isToggledOn: !previousState.isToggledOn
         }));
     }
 
-    expandText(){
-        console.log(this.props.children);
-        return (this.props.children)
+    expandText = () => {
+        console.log("color change");
+
+        /*$(this.props.children).css("color", "red")*/
+
+        // return (this.props.children)
     }
 
     render(){
         return(
             <Row className="collapsible-group">
+                <div className={this.state.isToggledOn ? "randomItem active" : "randomItem"}></div>
+
                 <div onClick={this.isClicked} className="expand-button">+</div>
-                <p className="group-content">{this.state.isToggledOn ? this.expandText(): ""}</p>
+                
+                <p className={this.state.isToggledOn ? "group-content active":"group-content"}>
+                    {this.props.children}
+                </p>
+                
+                
             </Row>
         );
     }
