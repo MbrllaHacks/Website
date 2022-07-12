73bd62f39bd003d5b263f75591ee76c8bb8827b3
Samyak <73217093+samyak112@users.noreply.github.com>
2022-07-12 20:47:08 +0530

    non responsive home section with blur

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,6 +1,9 @@
 import "./home.css"
 import prin from "./../../Assets/prin.svg"
 import nick from "./../../Assets/nick.svg"
+import home_blur from './../../Assets/home_blur.svg'
+import plus from './../../Assets/plus.svg'
+import umbrella from './../../Assets/umbrella_2.svg'
 import {Container, Row, Col, Particle} from "react-bootstrap"
 
 
@@ -10,7 +13,7 @@ function Home(){
         <Container id="home">
           <Container className="home-section">
             <Row>
-                <Col lg={7} >
+                <Col lg={6} id='home_left' >
 
                     {/* Heading */}
 
@@ -22,6 +25,7 @@ function Home(){
                     <div className="dates">
                         OCT 08 - OCT 09
                     </div>
+                    
 
                     {/* Register Button */}
     
@@ -35,11 +39,55 @@ function Home(){
 
                     {/* Nick image */}
 
-                <Col lg={5}>
+                <Col lg={6} id='images'>
+                    <img src={home_blur} 
+                    alt="Blur behind nick holding a computer"
+                    className="img-fluid home_blur"
+                    ></img>
+
+                    <img src={home_blur} 
+                    alt="Blur behind nick holding a computer"
+                    className="img-fluid home_blur_3"
+                    ></img>
+
+                    <img src={home_blur} 
+                    alt="Blur behind nick holding a computer"
+                    className="img-fluid home_blur_2"
+                    ></img>
+
                     <img src={nick} 
                     alt="Nick holding a computer"
-                    className="img-fluid home-nick"
+                    className="home-nick"
                     ></img>
+
+                    <div className="float_imgs_top">
+                        <img src={plus} 
+                            alt="plus sign"
+                            className="img-fluid plus"
+                        ></img>
+                        <img src={umbrella} 
+                            alt="umbrella"
+                            className="img-fluid umbrella"
+                        ></img>
+                    </div>
+
+                    <div className="float_imgs_mid">
+                        <img src={plus} 
+                            alt="plus sign"
+                            className="img-fluid plus"
+                        ></img>
+                    </div>
+
+                    <div className="float_imgs_bottom">
+                        <img src={umbrella} 
+                            alt="umbrella"
+                            className="img-fluid umbrella"
+                        ></img>
+                        <img src={plus} 
+                            alt="plus sign"
+                            className="img-fluid plus"
+                        ></img>
+                    </div>
                 </Col>
             </Row>
 
