9db373148d40feea9bf4c8ccb6165075d4bcd158
Hiten1708 <68720970+Hiten1708@users.noreply.github.com>
2022-07-15 23:57:15 -0400

    Removed and renamed some images

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -29,7 +29,7 @@ function About(){
 
                     <img src={logo} alt="logo" className="about-logo" width="500px" ></img>
 
-                    <p class="about-text">
+                    <p className="about-text">
                         A student-run hackathon with the goal to empower hackers of all levels to turn their ideas into reality, with the help of the workshops and resources we make available. In 48 hours, you will be able to develop a project, network with other students from all over the world and learn together!
                     </p>
 
@@ -37,7 +37,7 @@ function About(){
                         Hello World!
                     </div>
 
-                    <p class="about-text">
+                    <p className="about-text">
                     We will be conducting several workshops to provide anyone with the chance to learn about multiple skills and resources that will help turn ideas into a real project! 
                     <br/>
                     Beginner-level hackers should <span className="blue">implement what they learned from the workshops </span> into their proposal!
