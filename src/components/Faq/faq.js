056e6ce56a78c00ee203672bf6d818cf3fcd74bd
amishasao <105593970+amishasao@users.noreply.github.com>
2022-07-18 21:52:36 -0500

    Completed faq (#81)
    * git commit src/components/Faq/faq.css
    git commit src/components/Faq/faq.js
    git commit src/components/Faq/questions.js
    
    * git commit src/components/Faq/questions.css
    
    * Update .gitignore (#66)
    
    Co-authored-by: Hitenkumar Patel <68720970+Hiten1708@users.noreply.github.com>

--- src/components/Faq/faq.js
+++ src/components/Faq/faq.js
@@ -5,6 +5,18 @@ import ToggleableAnswer from "./questions.js";
 import crossPattern from "./../../Assets/crossPattern.png";
 
 function Faq(){
+    var qAndA_Left = {"What is Mbrlla Hacks?": "Mbrlla Hacks is a virtual hackathon, where hackers of all levels and backgrounds team up to create a coding project from scratch in just 48 hours. Alongside this, we provide participants with workshops and other resources to help them turn their ideas into a reality.",
+                "Does this cost money?":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat eaque at amet! Nobis, natus? Excepturi ea consectetur voluptate ipsa iusto aut. Aspernatur, veritatis. Ut exercitationem officia harum ullam corrupti.",
+                "What if this is my first time at a hackathon?":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!",
+                "What kinds of events will be there?":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!",
+                "When do hacker applications open?":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!"}
+
+    var qAndA_Right = {"Who can participate?": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores nesciunt eligendi, ratione necessitatibus aliquam tempore. Expedita iste alias consequuntur minima. Libero saepe exercitationem quam accusamus dignissimos deleniti? Mollitia, reiciendis!",
+                "How many people can be on a team?":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat eaque at amet! Nobis, natus? Excepturi ea consectetur voluptate ipsa iusto aut. Aspernatur, veritatis. Ut exercitationem officia harum ullam corrupti.",
+                "How do I form a team?":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!",
+                "What is the judging criteria?":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!",
+                "Are there prizes?":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!"}
+
     return(
         <section>
             <Container className="faq-section" id="faq">
@@ -13,77 +25,29 @@ function Faq(){
                     <h1 id="faq-title" className="title">Frequently Asked Questions</h1>
 
                     <Row className="faq-body">
-                        <Col id="col1">
-                            <div id="faq-question">
-                                What is Mbrlla Hacks?
-                            </div>
-                            <ToggleableAnswer className="faq-answer">
-                                Mbrlla Hacks is a virtual hackathon, where hackers of all levels and backgrounds team up to create a coding project from scratch in just 48 hours. Alongside this, we provide participants with workshops and other resources to help them turn their ideas into a reality.
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                Does this cost money?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quaerat eaque at amet! Nobis, natus? Excepturi ea consectetur voluptate ipsa iusto aut. Aspernatur, veritatis. Ut exercitationem officia harum ullam corrupti.
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                What if this is my first time at a hackathon?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                What kinds of events will be there?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis eos nesciunt magni impedit. Accusantium placeat quos vero asperiores voluptatem? Deserunt eaque hic nihil magnam minima, impedit id eveniet sed porro?
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                When do hacker applications open?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. In omnis culpa eveniet, nisi veritatis repudiandae quis facere, vero molestiae vel ratione a fugiat qui debitis, corporis harum beatae placeat excepturi!                                
-                            </ToggleableAnswer>
+                        <Col id="col1" lg={6}>
+                            {Object.entries(qAndA_Left).map(([key, value]) => (
+                                <div>
+                                    <div id="faq-question">
+                                        {key}
+                                    </div>
+                                    <ToggleableAnswer className="faq-answer">
+                                        {value}
+                                    </ToggleableAnswer>
+                                </div>
+                            ))}
                         </Col>
-                        <Col>
-                            <div id="faq-question">
-                                Who can participate?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores nesciunt eligendi, ratione necessitatibus aliquam tempore. Expedita iste alias consequuntur minima. Libero saepe exercitationem quam accusamus dignissimos deleniti? Mollitia, reiciendis!
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                How many people can be on a team?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quod laudantium natus rerum illo incidunt ab quisquam, et recusandae porro, earum dignissimos culpa dolor. Ipsam eaque nostrum amet doloremque quos.
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                How do I form a team?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero cumque accusantium provident voluptate optio odio doloribus maiores minima perferendis magnam cupiditate eveniet, laborum qui magni. Facere nostrum eaque earum beatae.
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                What is the judging criteria?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid eveniet tempora omnis maiores quae dolorum ad amet obcaecati error nemo, dolores voluptatibus veniam corrupti. Minima ab accusantium adipisci asperiores voluptatibus?
-                            </ToggleableAnswer>
-
-                            <div id="faq-question">
-                                Are there prizes?
-                            </div>
-                            <ToggleableAnswer>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum delectus ab rem quos inventore officiis eum unde quis voluptatibus? Dolorum, fuga aliquam voluptas aliquid quia iure perferendis vel labore quibusdam!
-                            </ToggleableAnswer>
+                        <Col id="col2" lg={6}>
+                            {Object.entries(qAndA_Right).map(([key, value]) => (
+                                <div>
+                                    <div id="faq-question">
+                                        {key}
+                                    </div>
+                                    <ToggleableAnswer className="faq-answer">
+                                        {value}
+                                    </ToggleableAnswer>
+                                </div>
+                            ))}
                         </Col>
                     </Row>
                 </Container>
--- src/components/Faq/questions.js
+++ src/components/Faq/questions.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Row } from "react-bootstrap";
-import './faq.css';
+import { Container, Row, Col} from "react-bootstrap";
+import './questions.css';
 
 class ToggleableAnswer extends React.Component{
     constructor(props){
@@ -10,22 +10,36 @@ class ToggleableAnswer extends React.Component{
         this.isClicked = this.isClicked.bind(this);
     }
 
+    componentDidMount(){
+        this.expandText()
+    }
+
     isClicked(){
         this.setState(previousState => ({
             isToggledOn: !previousState.isToggledOn
         }));
     }
 
-    expandText(){
-        console.log(this.props.children);
-        return (this.props.children)
+    expandText = () => {
+        console.log("color change");
+
+        /*$(this.props.children).css("color", "red")*/
+
+        // return (this.props.children)
     }
 
     render(){
         return(
             <Row className="collapsible-group">
+                {/* <div className={this.state.isToggledOn ? "randomItem active" : "randomItem"}></div> */}
+
                 <div onClick={this.isClicked} className="expand-button">+</div>
-                <p className="group-content">{this.state.isToggledOn ? this.expandText(): ""}</p>
+                
+                <p className={this.state.isToggledOn ? "group-content active":"group-content"}>
+                    {this.props.children}
+                </p>
+                
+                
             </Row>
         );
     }
